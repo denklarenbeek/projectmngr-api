abcef446b251046804cbe228e8308a7b09ee8dbb
Dennis Klarenbeek <dennisklarenbeek@192.168.0.100>
2023-04-24 21:09:14 +0200

    added public origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,9 @@
 const allowedOrigins = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://projectmngr.onrender.com',
+    'http://projectmngr.onrender.com',
+    "http://projectmngr.nl",
+    "https://projectmngr.nl"
 ]
 
 module.exports = allowedOrigins;
